fbd06098761de70b3a13030fb5cef7432481fbeb
Nate Willis <natewillis1@gmail.com>
2021-02-08 15:21:18 -0600

    added raw messages

--- hyperion/scenarios/urls.py
+++ hyperion/scenarios/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<scenario_id>/raw_messages/', views.raw_messages, name='raw_messages')
 ]
--- hyperion/scenarios/views.py
+++ hyperion/scenarios/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render
-from .models import Scenario
+from .models import Scenario, Delivery, Shipment
 
 
 def index(request):
     latest_scenario_list = Scenario.objects.order_by('-exercise_start')[:5]
     context = {'latest_scenario_list': latest_scenario_list}
     return render(request, 'scenarios/index.html', context)
+
+
+def raw_messages(request, scenario_id):
+    shipments = Shipment.objects.filter(scenario_id=scenario_id).order_by('start_datetime')
+    deliveries = Delivery.objects.filter(shipment__scenario_id=scenario_id)
+
+    # init message dict
+    messages = {}
+
+    # shipment logic
+    for shipment in shipments:
+
+        # format the message key
+        message_key_int = 1
+        message_key = f'{shipment.start_datetime.strftime("%Y%m%d%H%M%S")}_{message_key_int:05d}'
+        while message_key in messages:
+            message_key_int += 1
+            message_key = f'{shipment.start_datetime.strftime("%Y%m%d%H%M%S")}_{message_key_int:05d}'
+
+        # create the message
+        message = f'{message_key} shipment from {shipment.country.name}'
+
+        # store the message
+        messages[message_key] = message
+
+    # delivery logic
+    for delivery in deliveries:
+
+        # format the message key
+        message_key_int = 1
+        message_key = f'{delivery.end_datetime.strftime("%Y%m%d%H%M%S")}_{message_key_int:05d}'
+        while message_key in messages:
+            message_key_int += 1
+            message_key = f'{delivery.end_datetime.strftime("%Y%m%d%H%M%S")}_{message_key_int:05d}'
+
+        # create the message
+        message = f'{message_key} delivery with a weight of {delivery.package_weight} lbs'
+
+        # store the message
+        messages[message_key] = message
+
+    context = {'messages': [messages[message_key] for message_key in sorted(messages.keys())]}
+    return render(request, 'scenarios/raw_messages.html', context)
+
+
